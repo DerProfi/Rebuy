c8ea20db3cfcaab8a595654115883daba07580a7
Leon <Leon@HD-MBP-127.fritz.box>
2023-02-14 12:00:12 +0100

    Modal base

---
+++ src/components/listOffer.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <ul class="list">
+      <li v-for="offer in offers" :key="offer.id" class="offer">
+        <img
+          v-if="offer.image"
+          :src="offer.image"
+          :fallback="store.imageDefault"
+          alt="Product image"
+        />
+        <img
+          v-else
+          src="https://dummyimage.com/600x400/74c0c8/fff&text=ReMarket"
+          :fallback="store.imageDefault"
+          alt="Dummy Image"
+        />
+        {{ offer.title }} ({{ offer.votes }} votes)
+        <button @click="store.upvoteOffer(offer.id)">Upvote</button>
+        <button @click="store.downvoteOffer(offer.id)">Downvote</button>
+        <!-- <router-link :to="`/offers/${offer.id}`">Details</router-link> -->
+        <button @click="openModul">Details</button>
+
+        <div class="modal" id="myModal">
+          <div class="modal-content">
+            <span class="close" @click="closeModal">&times;</span>
+            <h1>{{ offer.title }}</h1>
+            <img
+              v-if="offer.image"
+              :src="offer.image"
+              :fallback="store.imageDefault"
+              alt="Product image"
+            />
+            <img
+              v-else
+              src="https://dummyimage.com/600x400/74c0c8/fff&text=ReMarket"
+              :fallback="store.imageDefault"
+              alt="Dummy Image"
+            />
+            <p>{{ offer.description }}</p>
+            <a :href="createLink" target="_blank" id="link"
+              >Purchase on Rebuy</a
+            >
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { useStore } from "../stores/store";
+
+const store = useStore();
+const { offers } = storeToRefs(store);
+
+var modal = document.getElementById("myModal");
+
+function openModul() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+function createLink() {
+  let link = "https://www.rebuy.de/i,";
+  document.getElementById("link")?.setAttribute("href", link);
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  var modal = document.getElementById("myModal");
+
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: black;
+}
+.offer {
+  outline: 1px solid black;
+  margin: 20px;
+  img {
+    display: flex;
+    width: 350px;
+  }
+}
+.list {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  row-gap: 50px;
+  list-style: none;
+}
+.container {
+  max-width: 1400px;
+  margin: 0 auto;
+  outline: 1px solid red;
+}
+
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
--- src/views/ListPage.vue
+++ src/views/ListPage.vue
@@ -1,32 +1,14 @@
 <template>
   <div class="container">
     <h1>ReMarket Offers</h1>
-    <ul class="list">
-      <li v-for="offer in offers" :key="offer.id" class="offer">
-        <img
-          v-if="offer.image"
-          :src="offer.image"
-          :fallback="store.imageDefault"
-          alt="Product image"
-        />
-        <img
-          v-else
-          src="https://dummyimage.com/600x400/74c0c8/fff&text=ReMarket"
-          :fallback="store.imageDefault"
-          alt="Dummy Image"
-        />
-        {{ offer.title }} ({{ offer.votes }} votes)
-        <button @click="store.upvoteOffer(offer.id)">Upvote</button>
-        <button @click="store.downvoteOffer(offer.id)">Downvote</button>
-        <router-link :to="`/offers/${offer.id}`">Details</router-link>
-      </li>
-    </ul>
+    <list-offer />
   </div>
 </template>
 
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
 import { useStore } from "../stores/store";
+import listOffer from "../components/listOffer.vue";
 
 const store = useStore();
 const { offers } = storeToRefs(store);
@@ -35,25 +17,5 @@ const { offers } = storeToRefs(store);
 <style lang="scss" scoped>
 h1 {
   color: black;
-}
-.offer {
-  outline: 1px solid black;
-  margin: 20px;
-  img {
-    display: flex;
-    width: 350px;
-  }
-}
-.list {
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-between;
-  row-gap: 50px;
-  list-style: none;
-}
-.container {
-  max-width: 1400px;
-  margin: 0 auto;
-  outline: 1px solid red;
 }
 </style>
